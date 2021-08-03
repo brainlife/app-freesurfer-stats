a35ef57eb154cda4bcda4a676b3f4d7b4bde72d2
Brad Caron <bacaron@iu.edu>
2021-08-03 15:05:18 -0400

    reason: small cleanup

--- compute-stats.sh
+++ compute-stats.sh
@@ -2,40 +2,6 @@
 
 freesurfer=`jq -r '.freesurfer' config.json`
 parc=`jq -r '.parcellation' config.json`
-# ad=`jq -r '.ad' config.json`
-# fa=`jq -r '.fa' config.json`
-# md=`jq -r '.md' config.json`
-# rd=`jq -r '.rd' config.json`
-# ga=`jq -r '.ga' config.json`
-# ak=`jq -r '.ak' config.json`
-# mk=`jq -r '.mk' config.json`
-# rk=`jq -r '.rk' config.json`
-# ndi=`jq -r '.ndi' config.json`
-# isovf=`jq -r '.isovf' config.json`
-# odi=`jq -r '.odi' config.json`
-# myelin=`jq -r '.myelin' config.json`
-
-# mkdir -p tmp
-
-# # set metrics for every situation
-# echo "parsing input diffusion metrics"
-# if [[ $fa == "null" ]];
-# then
-# 	METRIC="ndi isovf odi"
-# elif [[ $ndi == "null" ]] && [ ! -f $ga ]; then
-# 	METRIC="ad fa md rd"
-# elif [[ $ndi == "null" ]] && [ -f $ga ]; then
-# 	METRIC="ad fa md rd ga ak mk rk"
-# elif [ -f $fa ] && [ -f $ndi ] && [ ! -f $ga ]; then
-# 	METRIC="ad fa md rd ndi isovf odi"
-# else
-# 	METRIC="ad fa md rd ga ak mk rk ndi isovf odi"
-# fi
-# echo "input diffusion metrics set"
-
-# if [[ ! $myelin == "null" ]]; then
-# 	METRIC=$METRIC+" myelin"
-# fi
 
 METRIC=(`ls *.nii.gz`)
 

a9aa0c8d251e900a213530da4f8806ace4050756
Brad Caron <bacaron@iu.edu>
2021-08-12 23:51:30 -0400

    Update handle_scaling.sh

--- handle_scaling.sh
+++ handle_scaling.sh
@@ -10,6 +10,17 @@ rd=`jq -r '.rd' config.json`
 ndi=`jq -r '.ndi' config.json`
 isovf=`jq -r '.isovf' config.json`
 odi=`jq -r '.odi' config.json`
+myelin=`jq -r '.myelin' config.json`
+T1=`jq -r '.T1' config.json`
+R1=`jq -r '.R1' config.json`
+M0=`jq -r '.M0' config.json`
+PD=`jq -r '.PD' config.json`
+MTV=`jq -r '.MTV' config.json`
+VIP=`jq -r '.VIP' config.json`
+SIR=`jq -r '.SIR' config.json`
+WF=`jq -r '.WF' config.json`
+
+qmri="T1 R1 M0 PD MTV VIP SIR WF"
 
 #### parse whether dti and NODDI are included or not ####
 echo "parsing input diffusion metrics"
@@ -21,7 +32,23 @@ elif [[ $ndi == "null" ]]; then
 else
 	METRIC="ad fa md rd ndi isovf odi"
 fi
+
+if [[ ! $myelin == "null" ]]; then
+	METRIC=$METRIC" myelin"
+fi
+
+if [[ ! $T1 == "null" ]]; then
+	for i in ${qmri}
+	do
+		met_tmp=$(eval "echo \$${i}")
+		if [ -f ${met_tmp} ]; then
+			METRIC=$METRIC" ${i}"
+		fi
+	done
+fi
 echo "input diffusion metrics set"
+
+
 
 #### loop through metrics and generate stats text files ####
 for MET in ${METRIC}
